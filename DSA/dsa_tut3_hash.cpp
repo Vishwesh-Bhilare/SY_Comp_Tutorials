c3806374ddd4bb697ad44ed263af1721199debd0
Vishwesh Bhilare <bhilarevishwesh@gmail.com>
2025-08-16 19:15:36 +0530

    Update dsa_tut3_hash.cpp

--- DSA/dsa_tut3_hash.cpp
+++ DSA/dsa_tut3_hash.cpp
@@ -2,22 +2,37 @@
 using namespace std;
 
 int main(){
-	int val, hash_val, servers, requests;
+	int hash_val, servers, requests, num_val = 0;
+    string val;
 	
 	cout << "Enter the no. of servers: "; cin >> servers;
 	cout << "\nEnter the no. of requests: "; cin >> requests;
 	
-	int hash_servers[servers] = {};
+	string hash_servers[servers];
+
+    // set '0' to all hash_map table
+    for (int i = 0; i < servers; i++){
+        hash_servers[i] = "0";
+    }
 	
 	cout << endl;
 	
 	for(int i = 0; i < requests; i++){
 		cout << "\nEnter the IP add. " << i+1 <<": "; cin >> val;
-		hash_val = val % servers;	// division method
-		
+		int j = val.size() - 1;
+        int k = 1;
+        num_val = 0;
+
+        while(val[j] != '.'){
+            num_val = num_val + (val[j] - '0')*k;	// - '0' because val[j] is char
+            k *= 10;
+            j--;
+        }
+        hash_val = num_val % servers;   // division method
+
 		// linear probing for collion resolution
 		int check = 0;
-		while (hash_servers[hash_val] != 0){
+		while (hash_servers[hash_val] != "0"){
 			hash_val = (hash_val + 1) % servers;
 			
 			check += 1;	// check for repetitions
@@ -30,6 +45,10 @@ int main(){
 	}
 	
 	for (int i = 0; i < servers; i++){
+        if (hash_servers[i] == "0"){
+            cout << "\nServer " << i+1 << " is not handling any request";
+            continue;
+        }
 		cout << "\nServer " << i+1 << " is handling " << hash_servers[i] << " request";
 	}
 	
