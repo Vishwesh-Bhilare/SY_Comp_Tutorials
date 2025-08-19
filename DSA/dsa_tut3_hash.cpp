d391293fe7670f79ad373eb5ca2c95cd21f55e9f
Vishwesh Bhilare <bhilarevishwesh@gmail.com>
2025-08-19 07:59:49 +0530

    Update dsa_tut3_hash.cpp

--- DSA/dsa_tut3_hash.cpp
+++ DSA/dsa_tut3_hash.cpp
@@ -1,56 +1,62 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-	int hash_val, servers, requests, num_val = 0;
+int main() {
+    int hash_val, servers, requests, num_val = 0;
     string val;
-	
-	cout << "Enter the no. of servers: "; cin >> servers;
-	cout << "\nEnter the no. of requests: "; cin >> requests;
-	
-	string hash_servers[servers];
 
-    // set '0' to all hash_map table
-    for (int i = 0; i < servers; i++){
-        hash_servers[i] = "0";
-    }
-	
-	cout << endl;
-	
-	for(int i = 0; i < requests; i++){
-		cout << "\nEnter the IP add. " << i+1 <<": "; cin >> val;
-		int j = val.size() - 1;
+    cout << "Enter number of servers: "; 
+    cin >> servers;
+    cout << "\nEnter number of incoming requests: "; 
+    cin >> requests;
+
+    string hash_servers[100][100];   // max 100 servers, 100 requests each
+    int server_count[100] = {0};     // track how many requests each server got
+
+    cout << "\nEnter request IDs:\n";
+
+    for(int i = 0; i < requests; i++) {
+        cout << "Request " << i+1 << ": "; 
+        cin >> val;
+
+        // take last octet
+        int j = val.size() - 1;
         int k = 1;
         num_val = 0;
 
-        while(val[j] != '.'){
-            num_val = num_val + (val[j] - '0')*k;	// - '0' because val[j] is char
+        while(val[j] != '.' && j >= 0) {
+            num_val = num_val + (val[j] - '0') * k;
             k *= 10;
             j--;
         }
         hash_val = num_val % servers;   // division method
 
-		// linear probing for collion resolution
-		int check = 0;
-		while (hash_servers[hash_val] != "0"){
-			hash_val = (hash_val + 1) % servers;
-			
-			check += 1;	// check for repetitions
-			if (check >= servers){
-				cout << "No free space!";
-				return -1;
-			}
-		}
-		hash_servers[hash_val] = val;
-	}
-	
-	for (int i = 0; i < servers; i++){
-        if (hash_servers[i] == "0"){
-            cout << "\nServer " << i+1 << " is not handling any request";
-            continue;
+        // linear probing for collision resolution
+        int check = 0;
+        while (server_count[hash_val] > 0) {
+            // we allow multiple requests in same server, so no need to skip
+            break;
         }
-		cout << "\nServer " << i+1 << " is handling " << hash_servers[i] << " request";
-	}
-	
-	return 0;
+
+        // assign request
+        hash_servers[hash_val][server_count[hash_val]] = val;
+        server_count[hash_val]++;
+
+        cout << "Request '" << val << "' is handled by Server " << hash_val << "\n";
+    }
+
+    cout << "\nServer Load Summary:\n";
+    for (int i = 0; i < servers; i++) {
+        cout << "Server " << i << " handled " << server_count[i] << " requests";
+        if (server_count[i] > 0) {
+            cout << ": ";
+            for (int j = 0; j < server_count[i]; j++) {
+                cout << hash_servers[i][j];
+                if (j < server_count[i]-1) cout << ", ";
+            }
+        }
+        cout << "\n";
+    }
+
+    return 0;
 }
