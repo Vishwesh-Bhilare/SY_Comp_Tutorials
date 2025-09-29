6f272e42c5b2dc9da26e6ee05944a76c1d76a4f1
Vishwesh Bhilare <bhilarevishwesh@gmail.com>
2025-09-29 19:20:00 +0530

    Update dsa_tut4_linked_list.cpp

--- DSA/dsa_tut4_linked_list.cpp
+++ DSA/dsa_tut4_linked_list.cpp
@@ -7,17 +7,17 @@ public:
     string taskName;
     int priority;
     float exec_time;
-    ListNode *next;
+    ListNode* next;
 };
 
-void sort_priority(ListNode* arr[], int n){
-int i, j;
-ListNode *key;
-for (i = 1; i < n; ++i) {
+void sort_priority(ListNode* arr[], int n) {
+    int i, j;
+    ListNode* key;
+    for (i = 1; i < n; ++i) {
         key = arr[i];
         j = i - 1;
 
-        while (j >= 0 && (arr[j])->priority > key->priority) {
+        while (j >= 0 && arr[j]->priority > key->priority) {
             arr[j + 1] = arr[j];
             j = j - 1;
         }
@@ -25,14 +25,14 @@ for (i = 1; i < n; ++i) {
     }
 }
 
-void sort_exec(ListNode* arr[], int n){
-int i, j;
-ListNode *key;
-for (i = 1; i < n; ++i) {
+void sort_exec(ListNode* arr[], int n) {
+    int i, j;
+    ListNode* key;
+    for (i = 1; i < n; ++i) {
         key = arr[i];
         j = i - 1;
 
-        while (j >= 0 && (arr[j])->exec_time > key->exec_time) {
+        while (j >= 0 && arr[j]->exec_time > key->exec_time) {
             arr[j + 1] = arr[j];
             j = j - 1;
         }
@@ -40,49 +40,54 @@ for (i = 1; i < n; ++i) {
     }
 }
 
-int main(){
-ListNode *head = nullptr;
-ListNode *tail = nullptr;
-ListNode *temp = nullptr;
+int main() {
+    ListNode* head = nullptr;
+    ListNode* tail = nullptr;
+    ListNode* temp = nullptr;
 
-int n;
-cout << "Enter the number of tasks to be added: "; cin >> n;
-ListNode* ptr_arr[n];
+    int n;
+    cout << "Enter the number of tasks to be added: ";
+    cin >> n;
+    ListNode* ptr_arr[n];
 
-for (int i = 0; i < n; i++){
-temp = new ListNode;
+    for (int i = 0; i < n; i++) {
+        temp = new ListNode;
 
-cout << "Enter task name: "; cin >> temp->taskName;
-cin.ignore();
-cout << "Enter task priority (1 - 10): "; cin >> temp->priority;
-cout << "Enter task execution time (ms): "; cin >> temp->exec_time;
+        cout << "Enter task name: ";
+        cin >> temp->taskName;
+        cin.ignore(); // To handle any extra newline characters
+        cout << "Enter task priority (1 - 10): ";
+        cin >> temp->priority;
+        cout << "Enter task execution time (ms): ";
+        cin >> temp->exec_time;
 
-if (head == nullptr){
-head = temp;
-tail = temp;
-} else{
-tail->next = temp;
-tail = temp;
-}
+        if (head == nullptr) {
+            head = temp;
+            tail = temp;
+        } else {
+            tail->next = temp;
+            tail = temp;
+        }
 
-ptr_arr[i] = temp;
-cout << "\n------------\n";
+        ptr_arr[i] = temp;
+        cout << "\n------------\n";
+    }
 
-}
+    // Sorting tasks by priority
+    sort_priority(ptr_arr, n);
 
-sort_priority(ptr_arr, n);
+    cout << "Tasks according to Priority: " << endl;
+    for (int i = 0; i < n; i++) {
+        cout << ptr_arr[i]->taskName << ": " << ptr_arr[i]->priority << endl;
+    }
 
-cout << "Tasks according to Priority: "<< endl;
-for (int i = 0; i < n; i++){
-cout << (ptr_arr[i])->taskName << ": " << (ptr_arr[i])->priority << endl;
-}
+    // Sorting tasks by execution time
+    sort_exec(ptr_arr, n);
 
-sort_exec(ptr_arr, n);
+    cout << "\nTasks according to Execution Time: " << endl;
+    for (int i = 0; i < n; i++) {
+        cout << ptr_arr[i]->taskName << ": " << ptr_arr[i]->exec_time << "ms" << endl;
+    }
 
-cout << "\nTasks according to Execution Time: "<< endl;
-for (int i = 0; i < n; i++){
-cout << (ptr_arr[i])->taskName << ": " << (ptr_arr[i])->exec_time << "ms" << endl;
-}
-
-return 0;
+    return 0;
 }
