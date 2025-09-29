0173bef876f860a5e482304a258a680a4a35bbee
Vishwesh Bhilare <bhilarevishwesh@gmail.com>
2025-09-29 21:38:32 +0530

    Update dsa_practical_1_rainfall.cpp

--- DSA/dsa_practical_1_rainfall.cpp
+++ DSA/dsa_practical_1_rainfall.cpp
@@ -6,3 +6,8 @@ using namespace std;
 void getinfo(int city, int month, rainfall[10], db[10][10]);
 
 int getavg(int city, int month, rainfall)
+
+/*
+  idk where the code is, bare with with it
+  link to code img for ref.: https://docs.google.com/document/d/1JdDHSdBOI1wYKkNKfF4VaVbzekfHAgONNKVxkJ6Arpg/edit?usp=sharing
+*/
