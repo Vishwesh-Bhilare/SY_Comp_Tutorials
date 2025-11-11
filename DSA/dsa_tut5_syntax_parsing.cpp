0fc1e20043e53fe4c851983585320be8e81322f1
Vishwesh Bhilare <bhilarevishwesh@gmail.com>
2025-11-11 10:06:54 +0530

    Update dsa_tut5_syntax_parsing.cpp

--- DSA/dsa_tut5_syntax_parsing.cpp
+++ DSA/dsa_tut5_syntax_parsing.cpp
@@ -1,57 +1,56 @@
 #include <iostream>
-#include <stack>
 #include <string>
 using namespace std;
-int prec(char c) {
-    if (c == '^')
-        return 3;
-    else if (c == '/' || c == '*')
-        return 2;
-    else if (c == '+' || c == '-')
-        return 1;
-    else
-        return -1;
-}
-string infixToPostfix(string &s) {
-    stack<char> st;
-    string res = "";
-    for (int i = 0; i < s.size(); i++) {
-        char c = s[i];
-        if ((c >= 'a' && c <= 'z') ||
-            (c >= 'A' && c <= 'Z') ||
-            (c >= '0' && c <= '9'))
-            res += c;
-        else if (c == '(')
-            st.push('(');
-        else if (c == ')') {
-            while (!st.empty() && st.top() != '(') {
-                res += st.top();
-                st.pop();
-            }
-            if (!st.empty() && st.top() == '(') {
-                st.pop();
-            }
-        }
-        else {
-            while (!st.empty() && st.top() != '(' &&
-                   (prec(st.top()) > prec(c) ||
-                   (prec(st.top()) == prec(c) && c != '^'))) {
-                res += st.top();
-                st.pop();
-            }
-            st.push(c);
-        }
-    }
-    while (!st.empty()) {
-        res += st.top();
-        st.pop();
-    }
-    return res;
-}
-int main() {
-    string exp;
-    cout << "Enter the expression: "; cin >> exp;
-   
-    cout << infixToPostfix(exp);
+
+class Stack {
+    char arr[50];
+    int topIndex;
+public:
+    Stack() { topIndex = -1; }
+    void push(char x) { arr[++topIndex] = x; }
+    void pop() { topIndex--; }
+    char top() { return arr[topIndex]; }
+    bool empty() { return topIndex == -1; }
+};
+
+int prec(char op) {
+    if (op == '+' || op == '-') return 1;
+    if (op == '*' || op == '/') return 2;
     return 0;
+}
+
+int main() {
+    string infix, postfix = "";
+    cout << "Enter infix: ";
+    cin >> infix;
+
+    Stack s;
+
+    for (char c : infix) {
+        if (c >= 'A' && c <= 'Z') 
+            postfix += c;
+        else if (c == '(')
+            s.push(c);
+        else if (c == ')') {
+            while (!s.empty() && s.top() != '(') {
+                postfix += s.top();
+                s.pop();
+            }
+            s.pop();
+        } 
+        else {
+            while (!s.empty() && prec(s.top()) >= prec(c)) {
+                postfix += s.top();
+                s.pop();
+            }
+            s.push(c);
+        }
+    }
+
+    while (!s.empty()) {
+        postfix += s.top();
+        s.pop();
+    }
+
+    cout << "Postfix: " << postfix << endl;
 }
